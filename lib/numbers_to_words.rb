d0c0b34fab952e1e0fc02378ef040018d8e36f28
Alec Arme & Jim Patt <student@epicodus.com>
2015-04-21 15:50:56 -0700

    Removed first spec - no longer applicable. Second (0-9) and third (concat) specs pass.

--- lib/numbers_to_words.rb
+++ lib/numbers_to_words.rb
@@ -4,7 +4,7 @@ class Fixnum
     single_digits = { 1 => "one", 2 => "two", 3 => "three", 4 => "four", 5 => "five", 6 => "six", 7 => "seven", 8 => "eight", 9 => "nine" }
     number_container = self.to_s().split('')
     new_number_container = []
-
+    final_output = ""
 
     number_container.each() do |num_convert|
       new_number_container.push(num_convert.to_i())
@@ -15,10 +15,14 @@ class Fixnum
       if number.eql?(0)
         return zero_digit.fetch(number)
       else
-        return single_digits.fetch(number)
+        final_output = final_output.concat(single_digits.fetch(number).concat(" "))
+
       end
 
     end
+  final_output = final_output[0..(final_output.length().-(2))]
+  return final_output
+
 
   end
 
--- spec/numbers_to_words_spec.rb
+++ spec/numbers_to_words_spec.rb
@@ -3,11 +3,11 @@ require('numbers_to_words')
 require('pry')
 
 describe('Fixnum#numbers_to_words') do
-#Numbers to words takes a number and converts it in to an array with as many elements as there are digits.
+# NO LONGER APPLICABLE Numbers to words takes a number and converts it in to an array with as many elements as there are digits.
 
-  it("splits a multi digit number into an array") do
-    expect(451.numbers_to_words()).to(eq([4,5,1]))
-  end
+  # NO LONGER APPLICABLE it("splits a multi digit number into an array") do
+  #   expect(451.numbers_to_words()).to(eq([4,5,1]))
+  # end
 
 #Numbers to words takes a number from 0 to 9 and converts it to a word.
 
@@ -21,6 +21,9 @@ describe('Fixnum#numbers_to_words') do
 #Numbers to words takes  from a multiple of 10 (starting at 20) through 90 and converts it to a word.
 
 #Numbers combines the words generated into a single sentence representing the origional number.
+  it("concats the converted number in word form and adds them to the sentence.") do
+    expect(369.numbers_to_words()).to(eq("three six nine"))
+  end
 
 #Numbers to words checks the number length and adds the word "hundred", "thousand", etc when applicable.
   #this needs to be cascading - IE Ten Thousand still needs "thousand" and "hundred"
