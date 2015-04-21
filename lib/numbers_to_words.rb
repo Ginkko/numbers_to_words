f249761be1b074ae4b51bf2bc3fadc99323749c1
Alec Arme & Jim Patt <student@epicodus.com>
2015-04-21 14:52:06 -0700

    Pass 0-9 to word spec. Breaks first spec (digit to array) but will resolve when concatonate spec completed.

--- lib/numbers_to_words.rb
+++ lib/numbers_to_words.rb
@@ -1,6 +1,18 @@
 class Fixnum
   define_method(:numbers_to_words) do
-    self.to_s().split('')
+    zero_digit = { "0" => "zero" }
+    single_digits = { "1" => "one", "2" => "two", "3" => "three", "4" => "four", "5" => "five", "6" => "six", "7" => "seven", "8" => "eight", "9" => "nine" }
+    number_container = self.to_s().split('')
+
+    number_container.each() do |number|
+
+      if number.eql?("0")
+        zero_digit.fetch(number)
+      else
+        single_digits.fetch(number)
+      end
+
+    end
 
 
 
