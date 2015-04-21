6f536949b4ab2c73f7ebd5a327a8c04c65d5a5dc
Alec Arme & Jim Patt <student@epicodus.com>
2015-04-21 13:41:54 -0700

    Add plain text specs, build method shell.

---
+++ spec/numbers_to_words_spec.rb
@@ -0,0 +1,20 @@
+require('rspec')
+require('numbers_to_words')
+require('pry')
+
+describe('Fixnum#numbers_to_words') do
+#Numbers to words takes a number from 0 to 9 and converts it to a word.
+
+#Numbers to words takes a number from 11-19 and converts it to a word.
+
+#Numbers to words takes  from a multiple of 10 (starting at 20) through 90 and converts it to a word.
+
+#Numbers combines the words generated into a single sentence representing the origional number.
+
+#Numbers to words checks the number length and adds the word "hundred", "thousand", etc when applicable.
+  #this needs to be cascading - IE Ten Thousand still needs "thousand" and "hundred"
+
+
+
+
+end
